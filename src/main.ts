44c8c4e5ec036284e30dee00184e7b4b19a06a52
Jose Castellano <jcastellanosierras@gmail.com>
2025-08-06 18:45:09 +0200

    feat(main): habilitar CORS en la aplicación para permitir solicitudes de diferentes orígenes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from '@/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
+  app.enableCors();
+
   // Enable global validation pipe
   app.useGlobalPipes(
     new ValidationPipe({
